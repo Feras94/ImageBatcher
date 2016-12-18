38346dcee7d29e3c60f690330eeb879b03cf0d9d
Feras Dawod <Feras_da94@hotmail.com>
2016-12-18 22:07:23 +0200

    design app layout

--- ImageBatcher/MainForm.Designer.cs
+++ ImageBatcher/MainForm.Designer.cs
@@ -28,20 +28,212 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtMaxSize = new System.Windows.Forms.TextBox();
+            this.btnBrowseInput = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.checkZip = new System.Windows.Forms.CheckBox();
+            this.comboSizeType = new System.Windows.Forms.ComboBox();
+            this.inputFileDialoge = new System.Windows.Forms.OpenFileDialog();
             this.SuspendLayout();
             // 
-            // Form1
+            // txtInput
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.txtInput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtInput.Location = new System.Drawing.Point(138, 18);
+            this.txtInput.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.ReadOnly = true;
+            this.txtInput.Size = new System.Drawing.Size(344, 27);
+            this.txtInput.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 21);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Choose Files";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(13, 314);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(318, 34);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStart.Location = new System.Drawing.Point(459, 314);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(112, 34);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancel.Location = new System.Drawing.Point(339, 314);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(112, 34);
+            this.btnCancel.TabIndex = 4;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 60);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 19);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Output Path";
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtOutput.Location = new System.Drawing.Point(138, 56);
+            this.txtOutput.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(344, 27);
+            this.txtOutput.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 97);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 19);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Max Size";
+            // 
+            // txtMaxSize
+            // 
+            this.txtMaxSize.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtMaxSize.Location = new System.Drawing.Point(138, 94);
+            this.txtMaxSize.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtMaxSize.Name = "txtMaxSize";
+            this.txtMaxSize.Size = new System.Drawing.Size(299, 27);
+            this.txtMaxSize.TabIndex = 7;
+            // 
+            // btnBrowseInput
+            // 
+            this.btnBrowseInput.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBrowseInput.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.btnBrowseInput.Location = new System.Drawing.Point(490, 18);
+            this.btnBrowseInput.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnBrowseInput.Name = "btnBrowseInput";
+            this.btnBrowseInput.Size = new System.Drawing.Size(81, 27);
+            this.btnBrowseInput.TabIndex = 11;
+            this.btnBrowseInput.Text = "Browse";
+            this.btnBrowseInput.UseVisualStyleBackColor = true;
+            this.btnBrowseInput.Click += new System.EventHandler(this.btnBrowseInput_Click);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.button4.Location = new System.Drawing.Point(490, 56);
+            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(81, 27);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "Browse";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // checkZip
+            // 
+            this.checkZip.AutoSize = true;
+            this.checkZip.Location = new System.Drawing.Point(138, 128);
+            this.checkZip.Name = "checkZip";
+            this.checkZip.Size = new System.Drawing.Size(107, 23);
+            this.checkZip.TabIndex = 13;
+            this.checkZip.Text = "Zip Images";
+            this.checkZip.UseVisualStyleBackColor = true;
+            // 
+            // comboSizeType
+            // 
+            this.comboSizeType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboSizeType.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.comboSizeType.FormattingEnabled = true;
+            this.comboSizeType.Items.AddRange(new object[] {
+            "Mega Bytes (MB)",
+            "Kilo Bytes (KB)",
+            "Bytes (B)"});
+            this.comboSizeType.Location = new System.Drawing.Point(444, 97);
+            this.comboSizeType.Name = "comboSizeType";
+            this.comboSizeType.Size = new System.Drawing.Size(127, 24);
+            this.comboSizeType.TabIndex = 14;
+            // 
+            // inputFileDialoge
+            // 
+            this.inputFileDialoge.Title = "Choose Files";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(584, 361);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Controls.Add(this.comboSizeType);
+            this.Controls.Add(this.checkZip);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.btnBrowseInput);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtMaxSize);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtInput);
+            this.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "MainForm";
+            this.Text = "Image Batcher";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtMaxSize;
+        private System.Windows.Forms.Button btnBrowseInput;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.CheckBox checkZip;
+        private System.Windows.Forms.ComboBox comboSizeType;
+        private System.Windows.Forms.OpenFileDialog inputFileDialoge;
     }
 }
 
--- ImageBatcher/MainForm.cs
+++ ImageBatcher/MainForm.cs
@@ -12,9 +12,35 @@ namespace ImageBatcher
 {
     public partial class MainForm : Form
     {
+        private List<string> _imagesPathsList;
+
         public MainForm()
         {
             InitializeComponent();
         }
+
+        private void btnBrowseInput_Click(object sender, EventArgs e)
+        {
+            if (inputFileDialoge.ShowDialog() != DialogResult.OK)
+                return;
+
+            _imagesPathsList = new List<string>(inputFileDialoge.FileNames);
+        }
+
+        private void UpdateInputFilesTextBox()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (var path in _imagesPathsList)
+            {
+                stringBuilder.Append(path);
+                stringBuilder.Append(';');
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }
